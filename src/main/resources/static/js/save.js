24c4a7fcdb70b9f2e5a89334c0d90fdaabfe89eb
rok92 <kyeongrok@naver.com>
2023-03-26 20:50:07 +0900

    update save.js, save.html

--- src/main/resources/static/js/save.js
+++ src/main/resources/static/js/save.js
@@ -3,6 +3,7 @@ const emailCheck = () => {
     const email = document.getElementById("memberEmail").value;
     console.log("입력값 : ", email);
     const checkResult = document.getElementById("check-result");
+    event.preventDefault();
     $.ajax({
         // 요청방식: post, url: "email-check", 데이터 : 이메일
         type: "post",
@@ -13,15 +14,22 @@ const emailCheck = () => {
         success: function(result){
             console.log("요청성공", result);
             if(result == "ok"){
-                console.log("사용가능한 이메일 입니다.");
-                checkResult.innerHTML = "사용가능한 이메일 입니다";
+                alert("사용가능한 이메일 입니다.");
+                checkResult.alert = "사용가능한 이메일 입니다";
             }else{
-                console.log("이미 사용중인 이메일 입니다.");
-                checkResult.innerHTML = "이미 사용중인 이메일 입니다.";
+                alert("이미 사용중인 이메일 입니다.");
+                checkResult.alert = "이미 사용중인 이메일 입니다.";
             }
         },
         error: function(err){
             console.log(err);
         }
     })
-}
+};
+$(document).ready(function(){
+    $('#join').click(function(){
+        alert("환영합니다");
+    })
+})
+
+
